c6f6d16bb345cf699e96544d1ed67363701a18f2
Matheus Mariano <matheus.mariano@bemind.com.br>
2016-08-17 14:51:31 -0300

    Inicia biblioteca do iOS
    Conecta bibliotecas do React Native.

---
+++ ios/BMDPedometer/BMDPedometer.h
@@ -0,0 +1,14 @@
+//
+//  BMDPedometer.h
+//  BMDPedometer
+//
+//  Created by Matheus Mariano on 17/08/16.
+//  Copyright © 2016 Matheus Mariano. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "RCTBridgeModule.h"
+
+@interface BMDPedometer : NSObject<RCTBridgeModule>
+
+@end
---
+++ ios/BMDPedometer/BMDPedometer.m
@@ -0,0 +1,14 @@
+//
+//  BMDPedometer.m
+//  BMDPedometer
+//
+//  Created by Matheus Mariano on 17/08/16.
+//  Copyright © 2016 Matheus Mariano. All rights reserved.
+//
+
+#import "BMDPedometer.h"
+#import "RCTBridge.h"
+
+@implementation BMDPedometer
+
+@end
