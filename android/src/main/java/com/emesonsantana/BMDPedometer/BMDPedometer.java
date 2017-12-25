c70c3fb9def66a3a7e858fece84ad7bfdf27082d
Sofya Lopachenok <lopachenock@yandex.ru>
2017-12-25 16:40:02 +0300

    Fix android pedometer

--- android/src/main/java/com/emesonsantana/BMDPedometer/BMDPedometer.java
+++ android/src/main/java/com/emesonsantana/BMDPedometer/BMDPedometer.java
@@ -23,7 +23,6 @@ public class BMDPedometer implements ReactPackage {
     return modules;
   }
 
-  @Override
   public List<Class<? extends JavaScriptModule>> createJSModules() {
   	return Collections.emptyList();
   }
